6faf63e266084921d51371fa287d07f31e14ed63
austindoeswork <wilsonaustin17@gmail.com>
2017-05-05 14:03:52 -0400

    wip symmetry classifier

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,9 +1,23 @@
 from PIL import Image
 import numpy as np
 
-def showface(f):
-    img1 = Image.fromarray(f)
-    img1.show()
+def showface(f, features):
+    fscaled = f*255
+
+    w, h = 64, 64
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    for i in range(64):
+        for j in range(64):
+            data[i, j] = [0, fscaled[i,j], 0]
+
+    featscaled = features * 255
+    for i in range(64):
+        for j in range(64):
+            if featscaled[i,j] > 0.0:
+                data[i, j] = [featscaled[i,j], 0, featscaled[i,j]]
+
+    img = Image.fromarray(data, 'RGB')
+    img.show()
 
 def facearrtomatrix(np_arr):
     f = np_arr.reshape(64,64)
@@ -11,22 +25,45 @@ def facearrtomatrix(np_arr):
 
     return f
 
-faceclass = np.genfromtxt('../resources/faceclass.csv', delimiter=',')
-faceclass = faceclass[1:]
+def normalize(np_arr):
+    maxval = 0.0
+    for i in range(0, np.shape(np_arr)[0]):
+        if np_arr[i] > maxval:
+            maxval = np_arr[i]
+    for i in range(0, np.shape(np_arr)[0]):
+        np_arr[i] = np_arr[i] / maxval
 
-facedata = np.genfromtxt('../resources/facedata.csv', delimiter=',')
-faceindex = facedata[:,0]
-facedata = facedata[1:]
-facedata = facedata[:,1:]
+def getaverage(f, row, col, radius):
+    total = 0.0
+    count = 0.0
+    for i in range(row-radius, row+radius + 1):
+        for j in range(col-radius, col+radius + 1):
+            total += f[i,j]
+            count += 1.0
+    return total / count
 
-for i in range(0,20):
-    f = facearrtomatrix(facedata[i])
+def main():
+    faceclass = np.genfromtxt('../resources/faceclass.csv', delimiter=',')
+    faceclass = faceclass[1:]
+
+    facedata = np.genfromtxt('../resources/facedata.csv', delimiter=',')
+    facedata = facedata[1:]
+    faceindex = facedata[:,0]
+    facedata = facedata[:,1:]
+
+    print "FACEDATA :", np.shape(facedata)
+    print "FACEINDEX:", np.shape(faceindex)
+    print "FACECLASS:", np.shape(faceclass)
+
+    # NORMALIZE THE DATA
+    for i in range(0,np.shape(facedata)[0]):
+        normalize(facedata[i])
+
+    f = facearrtomatrix(facedata[0])
     showface(f)
 
-#  w, h = 512, 512
-#  data = np.zeros((h, w, 3), dtype=np.uint8)
-#  data[256, 256] = [255, 0, 0]
-#  img = Image.fromarray(data, 'RGB')
-#  img.show()
 
-#  img.save('my.png')
+if __name__ == "__main__":
+    main()
+
+
