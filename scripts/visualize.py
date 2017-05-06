93cb63b575c7611ad2475f354816d74d81c37261
austindoeswork <wilsonaustin17@gmail.com>
2017-05-06 15:59:32 -0400

    WIP convolution

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -17,6 +17,26 @@ def showface(f, features = np.zeros((64, 64), dtype=np.uint8)):
                 data[i, j] = [featscaled[i,j], 0, featscaled[i,j]]
 
     img = Image.fromarray(data, 'RGB')
+    img = img.resize((1024,1024))
+    img.show()
+
+def showfacemini(f, features = np.zeros((16, 16), dtype=np.uint8)):
+    fscaled = f*255
+
+    w, h = 16, 16
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    for i in range(16):
+        for j in range(16):
+            data[i, j] = [0, fscaled[i,j], 0]
+
+    featscaled = features * 255
+    for i in range(16):
+        for j in range(16):
+            if featscaled[i,j] > 0.0:
+                data[i, j] = [featscaled[i,j], 0, featscaled[i,j]]
+
+    img = Image.fromarray(data, 'RGB')
+    img = img.resize((1024,1024))
     img.show()
 
 def facearrtomatrix(np_arr):
@@ -37,17 +57,33 @@ def convolute(f, method="boxblur"):
     if method == "boxblur":
         cmatrix = np.ones((3,3), np.float64)/9
         buf = 1
-        print cmatrix 
+        #  print cmatrix 
     if method == "edge":
         cmatrix = np.matrix([[0.0,1.0,0.0],[1.0,-4.0,1.0],[0.0,1.0,0.0]])
         #  cmatrix = np.matrix([[-1.0,-1.0,-1.0],[-1.0,8.0,-1.0],[-1.0,-1.0,-1.0]])
         buf = 1
         print cmatrix 
-    fcopy = f.copy()
-    for i in range(buf, 64-buf):
-        for j in range(buf, 64-buf):
-            fcopy[i,j] = np.dot(cmatrix, f[i-buf:i+buf+1,j-buf:j+buf+1])[1,1]
+    #  fcopy = f.copy()
+    fcopy = np.zeros_like(f)
+    size = np.shape(f)[0]
+    for i in range(buf, size-buf):
+        for j in range(buf, size-buf):
+            #  fcopy[i,j] = np.dot(cmatrix, f[i-buf:i+buf+1,j-buf:j+buf+1])[1,1]
+            val = np.dot(cmatrix, f[i-buf:i+buf+1,j-buf:j+buf+1])[1,1]
+            #  print val
+            if val > -0.8:
+            #  if val < -1.5:
+                fcopy[i,j] = val
     return fcopy
+
+def downsample(f):
+    fdown = np.zeros((16,16), np.float64)
+    for i in range(0,16):
+        for j in range(0,16):
+            r = i * 4
+            c = j * 4
+            fdown[i,j] = f[r:r+4,c:c+4].mean()
+    return fdown
 
 def main():
     faceclass = np.genfromtxt('../resources/faceclass.csv', delimiter=',')
@@ -66,9 +102,18 @@ def main():
     for i in range(0,np.shape(facedata)[0]):
         normalize(facedata[i])
 
-    f = facearrtomatrix(facedata[0])
-    fconv = convolute(f, "edge")
-    showface(fconv)
+    #  featpos = np.zeros((16,16), np.uint8)
+    featpos = np.zeros((64,64), np.uint8)
+    for i in range(0,64):
+        featpos[i,0] = 1
+    for i in range(0,20):
+        f = facearrtomatrix(facedata[i])
+        #  fdown = downsample(f)
+        fconv = convolute(f, "edge")
+        if faceclass[i] == 1:
+            showface(fconv, featpos)
+        else:
+            showface(fconv)
     
 
 
