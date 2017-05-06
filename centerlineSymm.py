1982e54ec5632bbea6f97b7869f5fd320c191920
Wilson <wilson.g.gregory@gmail.com>
2017-05-06 15:56:00 -0400

    added pca components to training model

--- centerlineSymm.py
+++ centerlineSymm.py
@@ -6,6 +6,9 @@ import numpy as np
 import scipy
 from PIL import Image
 from sklearn.svm import LinearSVC
+from sklearn.decomposition import PCA
+import random
+from functools import reduce
 
 #const that specifies row (and col) size
 SIZE = 64
@@ -112,28 +115,31 @@ def normalizeFace(face,centerline):
     leftsum = float(leftsum) / (centerline*SIZE)
     rightsum = float(rightsum) / ((SIZE-centerline)*SIZE)
 
+    normFace = np.zeros((SIZE,SIZE), dtype=np.int)
+
     for i in range(0,SIZE):
         for j in range(0,centerline):
-            face[i,j] = face[i,j] - leftsum
+            normFace[i,j] = face[i,j] - leftsum
 
     for i in range(0,SIZE):
         for j in range(centerline,SIZE):
-            face[i,j] = face[i,j] - rightsum
-    
+            normFace[i,j] = face[i,j] - rightsum
+
+    return normFace
 
 #updated func
 def findCenterline(face,epsilon,start):
 
     maxSymm = 0
     maxSymmCenter = 32
-
+    #bestFace
     
     for i in range(start,SIZE-start): #will iterate over all possible centerlines
     #for i in range(32,33):
         totSymm = 0
-        normalizeFace(face,i)
+        normFace = normalizeFace(face,i)
         for j in range(0,SIZE): #j is the row
-            count, faceRow = calcSymm(face[j],i,epsilon)
+            count, faceRow = calcSymm(normFace[j],i,epsilon)
             totSymm += count     
         #end inner for loop
         
@@ -142,30 +148,73 @@ def findCenterline(face,epsilon,start):
         if(symmPercent > maxSymm):
             maxSymm = symmPercent
             maxSymmCenter = i
+            bestFace = normFace
 
     #update the graph with the best centerline
     #for j in range(0,SIZE):
     #    count, faceRow = calcSymm(face[j],maxSymmCenter,epsilon)
     #    face[j] = faceRow
-        
+    
+    #for i in range(0,SIZE):
+    #    face[i] = bestFace[i] #should correctly update
+    
     return (abs((SIZE/2)-maxSymmCenter)), maxSymm
 #end func
 
 
 #function that runs the SVM on the data, with the classes. Returns the score
-def runSVM(data,classes):
+def runSVM(traindata,trainclasses,testdata,testclasses):
     SVM = LinearSVC(dual=False)
-    SVM.fit(features,classes)
-    predictions = SVM.predict(features)
+    SVM.fit(traindata,trainclasses)
 
-    return SVM.score(features,classes)
+    return SVM.score(traindata,trainclasses), SVM.score(testdata,testclasses)
+
+
+#function that gets a random sample from the face data and puts it in a test set
+def sample(facedata, testSize, randList):
+    testSize = int(testSize)
+    facetrain = []
+    facetest = []
+    randListIndex = 0
+    for i in range(0,len(facedata)):
+        if(randListIndex < testSize and i == randList[randListIndex]):
+            facetest.append(facedata[i])
+            randListIndex += 1
+        else:
+            facetrain.append(facedata[i])
+    return facetrain, facetest
+#end func
+
+
+#Runs PCA on facedata and returns the data projected on the first numvecs eigenvectors
+def runPCA(facedata,testdata,numvecs):
+
+    data = []
+    #for i in range(0,len(facedata)):
+    #print(np.shape(facedata[i]))
+        
+    data = list(map((lambda x: x.reshape(1,4096)),facedata)) #what
+    data_arr = reduce((lambda x, y: np.concatenate((x,y),axis=0)), data)
+    
+    #print(data_arr)
+    
+    pca = PCA(n_components=numvecs)
+    pca.fit(data_arr)
+    results = pca.transform(data_arr)
+
+    data = list(map((lambda x: x.reshape(1,4096)),testdata)) #what
+    data_arr = reduce((lambda x, y: np.concatenate((x,y),axis=0)), data)
+    testresults = pca.transform(data_arr)
+    #print(np.shape(results),flush=True)
+    return results,testresults
+#end func
 
 
 #MAIN
 print("Entering Main:")
 
 
-#mustards code
+#mustards code. Imports data
 faceclass = np.genfromtxt('faceclass.csv', delimiter=',')
 faceclass = faceclass[1:] #trim the first row of faceclass
 
@@ -180,6 +229,8 @@ print(np.shape(facedata))
 
 epsilon = 13 #tunable, 13 was best on trainError, should recheck on test error (ECV)
 start = 20 #tunable 21 is best, might use 20 to be safe
+numpca = 10
+runs = 10
 
 allFaces = []
 for i in range(0,len(facedata)):
@@ -188,33 +239,60 @@ for i in range(0,len(facedata)):
 
 minError = len(allFaces)
 minIndex = 0
+testSize = len(allFaces) * 0.1
+totTestError = 0
 
-#print(len(allFaces))
-#print(np.shape(allFaces[0]))
-
-for q in range(0,1):
-    features = [] #reset features
+for q in range(0,runs):
+    trainfeatures = [] #reset features
+    testfeatures = []
     #epsilon = 5+q
     #start = 10+q
-    for i in range(0,len(allFaces)): #loop on each face
-        sim, dist = findCenterline(allFaces[i],epsilon,start )
-        features.append([sim,dist])
+    randList = random.sample(range(len(facedata)),int(testSize))
+    randList.sort()
+
+    facetrain, facetest = sample(allFaces, testSize,randList)
+    classtrain, classtest = sample(faceclass,testSize,randList)
+
+    trainVecs, testVecs = runPCA(facetrain,facetest,numpca)
+    #print(np.shape(trainVecs[0]))
+    for i in range(0,len(facetrain)): #loop on each face
+        sim, dist = findCenterline(facetrain[i],epsilon,start )
+        featlist = [sim,dist]
+        for j in range(0,numpca):
+            featlist.append(trainVecs[i,j])
+        trainfeatures.append(featlist)
     #end inner for loop
 
-    score = runSVM(features,faceclass)
-    error = 1-score
+    for i in range(0,len(facetest)): #loop on each face
+        sim, dist = findCenterline(facetest[i],epsilon,start )
+        featlist = [sim,dist]
+        for j in range(0,numpca):
+            featlist.append(testVecs[i,j])
+        testfeatures.append(featlist)
+    #end inner for loop
 
-    print("Train Error: " + str(error) +" at epsilon "+str(epsilon),
-          flush=True)
-    if(error < minError):
-        minError = error
+    trainscore,testscore = runSVM(trainfeatures,classtrain,testfeatures,classtest)
+    testerror = 1-testscore
+    trainerror = 1-trainscore
+
+    print("Train Error: " + str(trainerror))
+    print("Test Error: " + str(testerror), flush=True)
+    if(testerror < minError):
+        minError = testerror
         minIndex = epsilon
+    totTestError += testerror
+    
 #end for loop
 
-print("Best train error: " + str(minError) + " at epsilon " + str(minIndex))
-for i in range(0,10):
-    showface(allFaces[i])
+print("Average test error: " + str(float(totTestError) / runs))
+#for i in range(0,10):
+#    showface(allFaces[i])
 
+
+
+
+
+    
 #used for visualizing in R
 #for i in range(0,len(allFaces)):
 #    facedata[i] = matrixtofacearr(allFaces[i])
@@ -222,5 +300,5 @@ for i in range(0,10):
 #writeCSV(allFaces,labelsRow,'faces.csv')
 #writeCSV(features,['similarity','distance'],'features.csv')
 
-
+#might be better if we don't take bad pixels into the account when normalizing
 
