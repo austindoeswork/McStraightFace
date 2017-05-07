fa5a7498afdcbbad0742d328305a9dd4d3a60349
austindoeswork <wilsonaustin17@gmail.com>
2017-05-07 15:32:00 -0400

    Added Marker

--- scripts/visualize.py
+++ scripts/visualize.py
@@ -1,5 +1,8 @@
-from PIL import Image
+from PIL import Image , ImageTk, ImageDraw
+import Tkinter
 import numpy as np
+import random
+import math
 
 def showface(f, features = np.zeros((64, 64), dtype=np.uint8)):
     fscaled = f*255
@@ -19,6 +22,74 @@ def showface(f, features = np.zeros((64, 64), dtype=np.uint8)):
     img = Image.fromarray(data, 'RGB')
     img = img.resize((1024,1024))
     img.show()
+
+# left brow, left
+# left brow, top
+# left brow, right
+# right brow, left
+# right brow, top
+# right brow, right
+# left eye, left
+# left eye, center
+# left eye, right
+# right eye, left
+# right eye, center
+# right eye, right
+# nose, left
+# nose, left nostril
+# nose, tip
+# nose, right nostril
+# nose, right
+# mouth, left
+# mouth, top
+# mouth, bottom
+# mouth, right
+
+def markface(f, index):
+    # GET IMAGE
+    fscaled = f*255
+    w, h = 64, 64
+    data = np.zeros((h, w, 3), dtype=np.uint8)
+    for i in range(64):
+        for j in range(64):
+            data[i, j] = [0, fscaled[i,j], 0]
+    img = Image.fromarray(data, 'RGB')
+    img = img.resize((512,512))
+    imgname = "pictures/" + str(index) + ".png"
+    img.save(imgname)
+
+    #STORE THE MARKS
+    marks = np.zeros((22, 2), dtype=np.uint8)
+
+    def button_click_exit_mainloop (event):
+        rawx= event.x
+        rawy = event.y
+        x = int(math.floor(float(rawx)/8.0))
+        y = int(math.floor(float(rawy)/8.0))
+        print x, y
+        rowindex = marks[21,0]
+        marks[rowindex, 0] = x
+        marks[rowindex, 1] = y
+        #  print marks
+        marks[21, 0] = rowindex + 1
+        if rowindex + 1 == 21:
+            rowindex = 0
+            event.widget.quit() # this will cause mainloop to unblock.
+    root = Tkinter.Tk()
+    root.bind("<Button>", button_click_exit_mainloop)
+    root.geometry('512x512')
+    #load the image file we created before
+
+    tkpi = ImageTk.PhotoImage(file=imgname)
+    label_image = Tkinter.Label(root, image=tkpi)
+    label_image.pack()
+
+    #label_image.place(x=0,y=0,width=150,height=150)
+    #input()
+    root.mainloop()
+    root.destroy()
+
+    return marks[:21]
 
 def showfacemini(f, features = np.zeros((16, 16), dtype=np.uint8)):
     fscaled = f*255
@@ -91,8 +162,14 @@ def main():
 
     facedata = np.genfromtxt('../resources/facedata.csv', delimiter=',')
     facedata = facedata[1:]
-    faceindex = facedata[:,0]
-    facedata = facedata[:,1:]
+
+    # RANDOMIZE THE DATA
+    #  perm = np.random.permutation(facedata.shape[0])
+    #  facedata = facedata[perm, :]
+    #  faceclass = faceclass[perm]
+
+    faceindex = facedata[:,0] # get first col
+    facedata = facedata[:,1:] # remove first col
 
     print "FACEDATA :", np.shape(facedata)
     print "FACEINDEX:", np.shape(faceindex)
@@ -102,18 +179,37 @@ def main():
     for i in range(0,np.shape(facedata)[0]):
         normalize(facedata[i])
 
+    ptrain = 0.9
+    rows = np.shape(facedata)[0]
+    tindex = int(ptrain * float(rows))
+    DTrain = facedata[:tindex]
+    DTest = facedata[tindex:]
+    print "TRAIN :", np.shape(DTrain)
+    print "TEST:", np.shape(DTest)
+
+    
+    # TRAIN THE MARKS
+    for i in range(20):
+        if i % 10 == 0 or i % 10 == 1:
+            print "TRAINING INDEX", i
+            f = facearrtomatrix(facedata[i])
+            marks = markface(f, i)
+            print marks
+            np.savetxt("marks/" +str(i)+".csv", marks, delimiter=",", fmt="%d")
+
+    # RUN TESTS
     #  featpos = np.zeros((16,16), np.uint8)
-    featpos = np.zeros((64,64), np.uint8)
-    for i in range(0,64):
-        featpos[i,0] = 1
-    for i in range(0,20):
-        f = facearrtomatrix(facedata[i])
-        #  fdown = downsample(f)
-        fconv = convolute(f, "edge")
-        if faceclass[i] == 1:
-            showface(fconv, featpos)
-        else:
-            showface(fconv)
+    #  featpos = np.zeros((64,64), np.uint8)
+    #  for i in range(0,64):
+        #  featpos[i,0] = 1
+    #  for i in range(0,100):
+        #  f = facearrtomatrix(facedata[i])
+        #  #  fdown = downsample(f)
+        #  fconv = convolute(f, "edge")
+        #  if faceclass[i] == 1:
+            #  showface(fconv, featpos)
+        #  else:
+            #  showface(fconv)
     
 
 
